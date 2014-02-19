5c322c81f3d3e2c5f60324de2e946645b9d06e08
show555 <katokatonet+git@gmail.com>
2014-02-19 21:49:52 +0900

    最初のコミット

---
+++ Casks/jedit.rb
@@ -0,0 +1,7 @@
+class Jedit < Cask
+  url 'http://download.artman21.co.jp/product/JeditX/JeditX238.dmg'
+  homepage 'http://www.artman21.com/jp/jedit_x/'
+  version '2.38'
+  no_checksum
+  link 'Jedit X Folder/Jedit X.app'
+end
---
+++ Casks/openoffice-jp.rb
@@ -0,0 +1,7 @@
+class OpenofficeJp < Cask
+  url 'http://downloads.sourceforge.net/project/openofficeorg.mirror/4.0.1/binaries/ja/Apache_OpenOffice_4.0.1_MacOS_x86_install_ja.dmg'
+  homepage 'http://www.openoffice.org/'
+  version '4.0.1'
+  no_checksum
+  link 'OpenOffice.app'
+end
