cac4ff7b821c58fdfee825fadd6461dbcaf2dd1c
show555 <katokatonet+git@gmail.com>
2014-04-16 12:08:33 +0900

    Add ip-messenger cask

---
+++ Casks/ip-messenger.rb
@@ -0,0 +1,7 @@
+class IpMessenger < Cask
+  url 'http://ishwt.net/downloads/IPMessenger0B4.dmg'
+  homepage 'http://ishwt.net/software/ipmsg/'
+  version '0.11.4'
+  no_checksum
+  link 'IPMessenger.app'
+end
