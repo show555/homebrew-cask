712e17014a8b5f82c05227a50a3f81b5f38d69c3
show555 <katokatonet+git@gmail.com>
2014-05-07 18:07:12 +0900

    Update openoffice-jp cask

--- Casks/openoffice-jp.rb
+++ Casks/openoffice-jp.rb
@@ -1,7 +1,7 @@
 class OpenofficeJp < Cask
-  url 'http://downloads.sourceforge.net/project/openofficeorg.mirror/4.0.1/binaries/ja/Apache_OpenOffice_4.0.1_MacOS_x86_install_ja.dmg'
+  url 'http://downloads.sourceforge.net/project/openofficeorg.mirror/4.1.0/binaries/ja/Apache_OpenOffice_4.1.0_MacOS_x86-64_install_ja.dmg'
   homepage 'http://www.openoffice.org/'
-  version '4.0.1'
+  version '4.1.0'
   no_checksum
   link 'OpenOffice.app'
 end
